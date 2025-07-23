72c511a8280ecea39806d00f501ec466988b4f22
Pavithr jain <68775236+Pavithrj@users.noreply.github.com>
2025-07-23 08:13:39 +0000

    031

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ function App() {
             <h1>Vite + React</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
                 </button>
             </div>
         </>
